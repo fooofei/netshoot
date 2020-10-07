60a1628bb20c569c2833f088fb816082de7c47fc
fooofei <aihujianfei@qq.com>
2020-10-07 17:36:04 +0800

    fix no pushd command

--- scripts/build_ping.sh
+++ scripts/build_ping.sh
@@ -4,16 +4,14 @@ set -x
 
 home=$cur/build_ping
 mkdir -p $home
-pushd $home
+cd $home
 
 # at home dir
 git clone https://github.com/fooofei/go_pieces.git
 pkg_base_dir=$home/go_pieces/tool/xping
 go version
 
-pushd $pkg_base_dir
+cd $pkg_base_dir
 go build -v -tags netgo -o $cur/bin/tcping ./tcping/
 go build -v -tags netgo -o $cur/bin/httping ./httping/
-popd
 
-popd
