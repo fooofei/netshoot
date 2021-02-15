f0d560ed26e69c367d22ab3c5d46573c62c49b08
fooofei <aihujianfei@qq.com>
2021-02-15 13:14:07 +0800

    fix image push

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,16 +3,19 @@ cur=$(dirname "$(readlink -f $0)")
 set -x
 set -e
 
-sh -x  ${cur}/build_rustscan.sh
+bash -x  ${cur}/build_rustscan.sh
 
 # 以时间戳作为版本号
 # 格式为 20201007163924
 version=`date +"%Y%m%d%H%M%S"`
-image_name=swr.cn-north-4.myhuaweicloud.com/fooofei/netshoot:x86_64-${version}
+group_name="fooofei/netshoot"
+version_name="x86_64-${version}"
+image_name="${group_name}:${version_name}"
 
-cd ${cur}/..
+pushd ${cur}/..
 docker build . -t ${image_name}
-docker login -u cn-north-4@B1Q1DM5XDUUGNAHACWCE -p 3e70145984642dfb2d9073bb00b6b79f4910a9e90d340a37ca3ce1c231626ef2 swr.cn-north-4.myhuaweicloud.com
-docker push ${image_name}
+popd
+
+bash -x ${cur}/push_image.sh ${group_name} ${version_name}
 
 docker images
