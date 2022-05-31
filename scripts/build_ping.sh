5390216d463d13d8ae346110165e60b9767a9c72
fooofei <aihujianfei@qq.com>
2022-05-31 19:25:57 +0800

    fix build error

--- scripts/build_ping.sh
+++ scripts/build_ping.sh
@@ -12,9 +12,8 @@ pkg_base_dir=$home/go_pieces/tools/xping
 go version
 
 cd $pkg_base_dir
-go mod vendor 
-go build -v -mod=vendor -tags netgo -o /usr/local/bin/tcping ./tcping/
-go build -v -mod=vendor -tags netgo -o /usr/local/bin/httping ./httping/
+go build -v -tags netgo -o /usr/local/bin/tcping ./tcping/
+go build -v -tags netgo -o /usr/local/bin/httping ./httping/
 
 ls -al /usr/local/bin/tcping
 ls -al /usr/local/bin/httping
